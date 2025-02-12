ae11f8dec02ae04c9857fc79590df70444cd7b02
morachake <jacobmoracha@gmail.com>
2025-02-12 23:02:33 +0300

    adding a login view

--- authenthication/views.py
+++ authenthication/views.py
@@ -16,4 +16,30 @@ class RegisterView(generics.CreateAPIView)
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = UserSerializer
+    
+    def create(slef,request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception = True)
+        user = serializer.save()
+        token, created = Token.objects.get_or_create(user=user)
+        return Response ({
+            "user": UserSerializer(user,context=slef.get_serializer_context()).data,
+            "token": token.key
+        })
+class LoginView(generics.GenericAPIview):
+    serializer_class = LoginSerializer
+    permission_classes = (AllowAny)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = authenticate(email=serializer.validate_data['email'])
+        if user :
+            token,created = Token.objects.get_or_create(user=user)
+            return Response({
+                "user": UserSerializer(user,context=self.get_serializer_context()).data,
+                "token": token.key
+            })
+    return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
 
