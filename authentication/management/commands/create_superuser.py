04cbb8dda92471936e069dd9b23004666b0355b4
morachake <jacobmoracha@gmail.com>
2025-03-17 22:53:25 +0300

    Update superuser creation command to use email and add additional fields

--- authentication/management/commands/create_superuser.py
+++ authentication/management/commands/create_superuser.py
@@ -1,18 +1,25 @@
 # authentication/management/commands/create_superuser.py
 
 from django.core.management.base import BaseCommand
-from authentication.models import User  
+from authentication.models import User
 
 class Command(BaseCommand):
     help = 'Creates a superuser non-interactively if it doesn\'t exist'
 
     def handle(self, *args, **options):
-        username = 'limaAja@admin.com'
         email = 'limaAja@admin.com'
+        full_name = 'LimaAja Admin'
+        phone_number = '0701234567'
         password = 'LimaAja2025'
         
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username=username, email=email, password=password)
-            self.stdout.write(self.style.SUCCESS(f'Successfully created superuser: {username}'))
+        if not User.objects.filter(email=email).exists():
+            User.objects.create_superuser(
+                email=email, 
+                full_name=full_name,
+                phone_number=phone_number,
+                password=password,
+                is_verified=True
+            )
+            self.stdout.write(self.style.SUCCESS(f'Successfully created superuser with email: {email}'))
         else:
-            self.stdout.write(self.style.WARNING(f'Superuser {username} already exists'))
+            self.stdout.write(self.style.WARNING(f'Superuser with email {email} already exists'))
